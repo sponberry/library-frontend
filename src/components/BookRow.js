9d7244e27c07e24d76b68401fbc9fdf083c01846
abigailNottingham <abigailillustration@gmail.com>
2021-06-28 13:48:18 +0100

    ex 8.19 added filter by genre facility to books page

---
+++ src/components/BookRow.js
@@ -0,0 +1,28 @@
+import React from "react"
+
+const BookRow = ({book, genreFilter}) => {
+
+  if (genreFilter === "all genres") {
+    return (
+      <tr key={book.id}>
+        <td>{book.title}</td>
+        <td>{book.author.name}</td>
+        <td>{book.published}</td>
+      </tr>
+    )
+  }
+
+  if (book.genres.includes(genreFilter)) {
+    return (
+      <tr key={book.id}>
+            <td>{book.title}</td>
+            <td>{book.author.name}</td>
+            <td>{book.published}</td>
+          </tr>
+    )
+  }
+  
+  return null
+}
+
+export default BookRow
--- src/components/Books.js
+++ src/components/Books.js
@@ -1,8 +1,10 @@
 import { useQuery } from '@apollo/client'
-import React from 'react'
+import React, {useState} from 'react'
 import { ALL_BOOKS } from '../queries'
+import BookRow from "./BookRow"
 
 const Books = (props) => {
+  const [genreFilter, setGenreFilter] = useState("all genres")
   const result = useQuery(ALL_BOOKS)
 
   if (!props.show || result.loading) {
@@ -10,11 +12,21 @@ const Books = (props) => {
   }
 
   const books = result.data.allBooks
+  const allGenres = []
+  books.forEach(book => { book.genres.map(genre => allGenres.push(genre)) });
+  const uniqueGenres = [...new Set(allGenres)]
 
   return (
     <div>
       <h2>books</h2>
 
+      <p>filter by genre:</p>
+      <select id="name" value={genreFilter} onChange={(e) => setGenreFilter(e.target.value)}>
+            <option value="all genres">all genres</option>            
+            {uniqueGenres.map(genre => (
+              <option value={genre} key={genre}>{genre}</option>
+            ))}
+          </select>
       <table>
         <tbody>
           <tr>
@@ -27,12 +39,9 @@ const Books = (props) => {
             </th>
           </tr>
           {books.map(b =>
-            <tr key={b.id}>
-              <td>{b.title}</td>
-              <td>{b.author.name}</td>
-              <td>{b.published}</td>
-            </tr>
+            <BookRow book={b} genreFilter={genreFilter} key={b.id} />
           )}
+          
         </tbody>
       </table>
     </div>
